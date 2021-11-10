7ff7e600f8e9a4fec558f26af604b338ed1e9dbb
B3AL <92798364+B3AL@users.noreply.github.com>
2021-11-10 09:27:43 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,35 @@
+basic.forever(function () {
+    if (input.compassHeading() == 0) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    } else if (input.compassHeading() == 90) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    } else if (input.compassHeading() == 180) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    } else if (input.compassHeading() == 270) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    }
+})
